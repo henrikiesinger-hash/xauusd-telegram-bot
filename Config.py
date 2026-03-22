0364c5a7005d630aa1b7df344ebfc8c55d50e1ec
henrikiesinger-hash <henrikiesinger@web.de>
2026-03-22 20:06:02 +0700

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,11 @@
+import os
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+TWELVE_DATA_KEY = os.environ.get("TWELVE_DATA_KEY")
+
+SYMBOL = "XAU/USD"
+
+SIGNAL_SCORE_THRESHOLD = 8
+
+COOLDOWN_MINUTES = 30
