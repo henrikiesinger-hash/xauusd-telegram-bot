e089ff3e2bc67b870362abacbf21d5df1002a83c
henrikiesinger-hash <henrikiesinger@web.de>
2026-03-28 01:33:08 +0700

    filters.py aktualisieren

--- filters.py
+++ filters.py
@@ -5,33 +5,25 @@ last_signal_time = None
 
 
 def weekend_filter():
-
     day = datetime.utcnow().weekday()
-
     return day < 5
 
 
 def session_filter():
-
     hour = datetime.utcnow().hour
-
     return 7 <= hour <= 22
 
 
 def cooldown_filter():
-
     global last_signal_time
 
     if last_signal_time is None:
         return True
 
     diff = datetime.utcnow() - last_signal_time
-
     return diff.total_seconds() > COOLDOWN_MINUTES * 60
 
 
 def update_signal_time():
-
     global last_signal_time
-
     last_signal_time = datetime.utcnow()
